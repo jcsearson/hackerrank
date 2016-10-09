2e5a3013a43b969d3dc614f782f6b108cb64380c
James Charles Searson <jboned@jcsearson.local>
2016-10-09 10:50:18 -0400

    Indentation fixes (OCD)

--- C++ (solutions)/Cut the Sticks/cut-the-sticks.cpp
+++ C++ (solutions)/Cut the Sticks/cut-the-sticks.cpp
@@ -6,23 +6,23 @@
 using namespace std;
 int main(){
 	int n;
-
 	cin >> n;
 	int counter = n;
+	int old_count = arr.size();
 	vector<int> arr(n);
+
 	for(int arr_i = 0;arr_i < n;arr_i++){
-	   cin >> arr[arr_i];
+		cin >> arr[arr_i];
 	}
 	sort(arr.begin(), arr.end());
+	int el_min = arr[0];
 
-	int old_count = arr.size();
-    int el_min = arr[0];
 	while(!arr.empty()){
 		cout << counter << endl;
 		sort(arr.begin(), arr.end());
 
 		for(int k=0;k<old_count;k++){
-            el_min = arr[0];
+			el_min = arr[0];
 			if(el_min>=arr[k]){
 				arr.pop_back();
 				old_count = arr.size();
