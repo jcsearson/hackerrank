305954f8bd513cd73f1c92ad2d5add80b03e233e
James Charles Searson <jboned@jcsearson.local>
2016-10-09 10:47:14 -0400

    Initial commit.

---
+++ C++ (solutions)/Cut the Sticks/cut-the-sticks.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+	int n;
+
+	cin >> n;
+	int counter = n;
+	vector<int> arr(n);
+	for(int arr_i = 0;arr_i < n;arr_i++){
+	   cin >> arr[arr_i];
+	}
+	sort(arr.begin(), arr.end());
+
+	int old_count = arr.size();
+    int el_min = arr[0];
+	while(!arr.empty()){
+		cout << counter << endl;
+		sort(arr.begin(), arr.end());
+
+		for(int k=0;k<old_count;k++){
+            el_min = arr[0];
+			if(el_min>=arr[k]){
+				arr.pop_back();
+				old_count = arr.size();
+				counter--;
+			}
+			if(el_min<arr[k]){
+				arr[k] -= el_min;
+			}
+		}
+	}
+	return 0;
+}
